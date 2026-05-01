fa97d438f63c303efe76a7511dbe228a087bef41
Firee <newowner@News-MacBook-Pro.local>
2026-05-01 10:16:01 -0400

    add reset button

--- src/components/AdvancedCounter/CounterButtons.tsx
+++ src/components/AdvancedCounter/CounterButtons.tsx
@@ -1,2 +1,35 @@
 // COMPONENT JOB IS TO SHOW + and - buttons when clicked 
 
+interface CounterButtonsProps {
+
+    onIncrement: () => void;
+    onDecrement: () => void;
+    onReset: () => void;
+
+
+};
+
+
+function CounterButtons({ onIncrement, onDecrement }: CounterButtonsProps) {
+    return (
+        <div>
+            <button onClick={onIncrement}>
+                ➕ Increase
+            </button>
+
+            <button
+                onClick={onDecrement}
+                style={{ marginLeft: '8px' }}
+            >
+                ➖ Decrease
+
+            </button>
+
+            
+
+            <p style={{ color: 'gray' }}> ⚡️ Tip: ArrowUp and ArrowDown keys can also be used!</p>
+
+        </div>
+    );
+}
+export default CounterButtons;
